86f2f9cfb1f182407fbe50a3d80c362fa91836c6
Gabi Milani <dev02@linvix.com.br>
2023-11-27 13:46:23 -0300

    Adiciona erro na função de validar os prefixos do GTIN

--- src/ExportsAndTests/errorsList.ts
+++ src/ExportsAndTests/errorsList.ts
@@ -6,6 +6,7 @@ export const errorsList: {
   notString: `Invalid GTIN: The provided productCode is not of type string.`
   stringContainsNonDigitCharacters: "Invalid GTIN: The provided productCode contains non digit characters."
   invalidStringLengthForGTIN: `Invalid GTIN: Valid GTINs must 8 digits or 12-14 digits.`
+  invalidRegionPrefixForGTIN: `Invalid GTIN: GTIN region prefix is invalid.`
   invalidStringLengthForRemoveGTINLeadingZerosToUpcOrGTIN13: `Invalid productCode provided to removeGTINLeadingZerosToUpcOrGTIN13: Only valid GTINs between 12-14 digits are accepted.`
   invalidCheckDigit: `Invalid GTIN: GTIN check digit is invalid.`
   unknownRemoveGTINLeadingZerosToUpcOrGTIN13: "Unknown error attempting removeGTINLeadingZerosToUpcOrGTIN13 with the provided productCode."
@@ -18,6 +19,7 @@ export const errorsList: {
   stringContainsNonDigitCharacters:
     "Invalid GTIN: The provided productCode contains non digit characters.",
   invalidStringLengthForGTIN: `Invalid GTIN: Valid GTINs must 8 digits or 12-14 digits.`,
+  invalidRegionPrefixForGTIN: `Invalid GTIN: GTIN region prefix is invalid.`,
   invalidStringLengthForRemoveGTINLeadingZerosToUpcOrGTIN13: `Invalid productCode provided to removeGTINLeadingZerosToUpcOrGTIN13: Only valid GTINs between 12-14 digits are accepted.`,
   invalidCheckDigit: `Invalid GTIN: GTIN check digit is invalid.`,
   unknownRemoveGTINLeadingZerosToUpcOrGTIN13:
